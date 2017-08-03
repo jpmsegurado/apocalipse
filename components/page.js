b40e003a10c754214326ddff303bd1c87e7916be
Joao Pedro <joaopedro@Mac-mini-de-Joao.local>
2017-08-03 18:18:45 -0300

    tela de login

--- components/page.js
+++ components/page.js
@@ -5,7 +5,7 @@ const Body = styled.div`
 `;
 
 const Page = (props) => (
-  <Body className="container">
+  <Body className={props.withoutContainer ? '' : 'container'}>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"/>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
     {props.children}
--- pages/index.js
+++ pages/index.js
@@ -15,12 +15,44 @@ export default class People extends Component {
 
     render() {
         return (
-            <Page>
-                <div className="login">
-                    <div className="form-group">
-                        <div className="col-xs-12">
-                            <label>Usuário</label>
-                        </div>
+            <Page withoutContainer={true}>
+
+                <style jsx>
+                    {`
+                        .login {
+                            display: flex;
+                            position
+                            align-item: center;
+                        }
+
+                        label {
+                            height: 30px;
+                            margin: 0;
+                        }
+
+                        .login {
+                            position: absolute;
+                            margin: auto;
+                            top: 0;
+                            left: 0;
+                            bottom: 0;
+                            right: 0;
+                            height: 239px;
+                        }
+
+                    `}
+                </style>
+
+                <div className="col-xs-3 col-xs-offset-6 login">
+                    <div className="jumbotron">
+                            <div className="container">
+                                    <div className="form-group">
+                                    <label>Usuário</label>
+                                    <input className="form-control" type="text" placeholder="Seu ID"/>
+                                </div>
+
+                                <button className="btn btn-primary btn-block">Entrar</button>
+                            </div>
                     </div>
                 </div>
             </Page>
